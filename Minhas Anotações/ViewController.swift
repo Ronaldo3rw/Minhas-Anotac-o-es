fc8f382fbbcba532f4b37d7eefb923e97e8bf732
Ronaldo3rw <rdias3rw@gmail.com>
2018-02-04 11:52:04 -0200

    Aula 4/2/2018

--- Minhas Anotações/ViewController.swift
+++ Minhas Anotações/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Campo de texto para anotação
+    @IBOutlet weak var textoCampo: UITextView!
+    let chave = "minhaAnotacao"
 
+    
+    // Botão Salvar
+    @IBAction func btnSalvar(_ sender: Any) {
+        if let texto = textoCampo.text{
+            UserDefaults.standard.set(texto, forKey: chave)
+        }
+    }
+    
+    // Função pararecuperar a anotação
+    func recuperarAnotacao() -> String {
+        
+        if let textoRecuperado = UserDefaults.standard.object(forKey: chave) {
+            return textoRecuperado as! String
+        }
+        
+        return ""
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textoCampo.text = recuperarAnotacao()
+        
     }
 
     override func didReceiveMemoryWarning() {
