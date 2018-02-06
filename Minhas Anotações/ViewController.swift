705aabc2fafba63b086f07bc799dd9c3ec55ceda
Ronaldo3rw <rdias3rw@gmail.com>
2018-02-05 22:26:02 -0200

    Commit final de produção

--- Minhas Anotações/ViewController.swift
+++ Minhas Anotações/ViewController.swift
@@ -11,34 +11,46 @@ import UIKit
 class ViewController: UIViewController {
     // Campo de texto para anotação
     @IBOutlet weak var textoCampo: UITextView!
+    // Cria uma constante para armazenar a chave para acesso posterior
     let chave = "minhaAnotacao"
 
     
     // Botão Salvar
     @IBAction func btnSalvar(_ sender: Any) {
+        // recpera o que o usuário digitou no textoCampo
         if let texto = textoCampo.text{
+            // UserDefauts utilizando uma chave para salvar o texto
             UserDefaults.standard.set(texto, forKey: chave)
         }
     }
     
-    // Função pararecuperar a anotação
+    // Função para recuperar a anotação, retorna uma String
     func recuperarAnotacao() -> String {
-        
+        // Cria uma constante para recuperar a anotação utilizando a chave
         if let textoRecuperado = UserDefaults.standard.object(forKey: chave) {
-            return textoRecuperado as! String
+            // Se for possível recuperar o texto retorna textoRecuperado se não retorna uma string vazia
+            return textoRecuperado as! String // Faz um cast (conversão)
         }
-        
         return ""
-        
+    }
+    
+    // Método para esconder a barra de status
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
+    // Método para esconder o teclado
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        //print("Usuário pressionou a tela")
     }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         textoCampo.text = recuperarAnotacao()
-        
     }
 
     override func didReceiveMemoryWarning() {
